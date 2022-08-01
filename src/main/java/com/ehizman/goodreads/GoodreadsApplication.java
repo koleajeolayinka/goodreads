55ea511f7035c1cac5826ace1631635204cb2671
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-08-01 09:41:17 +0100

    ADDED FILES

---
+++ src/main/java/com/ehizman/goodreads/GoodreadsApplication.java
@@ -0,0 +1,13 @@
+package com.ehizman.goodreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodreadsApplication.class, args);
+	}
+
+}
